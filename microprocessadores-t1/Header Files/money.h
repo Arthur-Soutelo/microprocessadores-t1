2762ea24e1a8565060464bff14e020f0136890d3
Arthur <arthur.soutelo@gmail.com>
2024-07-06 22:06:05 -0300

    update schematic

--- microprocessadores-t1/Header Files/money.h
+++ microprocessadores-t1/Header Files/money.h
@@ -3,12 +3,12 @@
 #ifndef MONEY_H
 	#define MONEY_H
 
-	#define MONEY_PORT PORTA
-	#define MONEY_DIR  DDRA
-	#define MONEY_PIN  PINA
+	#define MONEY_PORT PORTH
+	#define MONEY_DIR  DDRH
+	#define MONEY_PIN  PINH
 
-	#define BUTTON1_PIN     PA0 // Assuming Button 1 is connected to PA0
-	#define BUTTON2_PIN     PA1 // Assuming Button 2 is connected to PA1
+	#define BUTTON1_PIN     PH5 // Assuming Button 1 is connected to PA0
+	#define BUTTON2_PIN     PH6 // Assuming Button 2 is connected to PA1
 
 	void buttons_init(void);
 	unsigned char button1_clicked(void);
