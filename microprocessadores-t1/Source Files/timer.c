c46539e876c83d50b76836706c420c844ffac891
Gabriel Mendes <gabriel.rosado-dos-santos-mendes@ecl21.ec-lyon.fr>
2024-07-27 18:31:22 -0300

    timer.c comments

--- microprocessadores-t1/Source Files/timer.c
+++ microprocessadores-t1/Source Files/timer.c
@@ -1,9 +1,9 @@
 #include "timer.h"
 
-unsigned short MY_VARIABLE_TCNT1 = 52202;
+unsigned short MY_VARIABLE_TCNT1 = 52202; // Valor inicial do contador do Timer1
 volatile uint8_t TIMEOUT_FLAG;
-volatile uint8_t seconds_count = 0;
-volatile uint16_t elapsed_time_2 = 0; // Track elapsed time
+volatile uint8_t seconds_count = 0; // Contador de segundos para o Timer1
+volatile uint16_t elapsed_time_2 = 0; // Contador de tempo decorrido para o Timer2
 
 
 // ================================= Timer 1 ================================= //
@@ -13,27 +13,27 @@ volatile uint16_t elapsed_time_2 = 0; // Track elapsed time
 void init_timer1(void) {
 	// Configure Timer1
 	TCCR1A = 0; // Normal mode
-	TCCR1B = (1 << WGM12) | (1 << CS12); // CTC mode, prescaler 256
-	OCR1A = 62499; // 1 second (16MHz / 256 / 1Hz)
-	TIMSK1 = (1 << OCIE1A); // Enable Timer1 compare interrupt
-	sei(); // Enable global interrupts
+	TCCR1B = (1 << WGM12) | (1 << CS12); // Modo CTC, prescaler 256
+	OCR1A = 62499; // 1 segundo (16MHz / 256 / 1Hz)
+	TIMSK1 = (1 << OCIE1A); // Habilita a interrupção de comparação do Timer1
+	sei(); // Habilita interrupções globais
 }
 
 ISR(TIMER1_COMPA_vect) {
 	seconds_count++;
 	if (seconds_count >= 30) {
-		TIMEOUT_FLAG = 1;
+		TIMEOUT_FLAG = 1; // Define a flag de timeout após 30 segundos utilizada para tempo máximo de inserção de moedas
 	}
 }
 
+// Parar Timer1
 void timer1_stop(void) {
-	// Parar Timer1
+	
 	TCCR1B &= ~((1<<CS12) | (1<<CS11) | (1<<CS10));
-	// Garantir que o pino PB5 esta em nivel baixo
-	PORTB &= ~(1<<PB5);
+	PORTB &= ~(1<<PB5);// Garantir que o pino PB5 esta em nivel baixo
 }
 
-
+// Reseta o contador de segundos e a flag de timeout
 void reset_timer_1(void) {
 	seconds_count = 0;
 	TIMEOUT_FLAG = 0;
@@ -45,85 +45,76 @@ void reset_timer_1(void) {
 
 // login timeout
 void timer2_init(void) {
-	// Set Timer2 to CTC mode
-	TCCR2A = (1 << WGM21);
-	// Set prescaler to 64 and start Timer2
-	TCCR2B = (1 << CS22);
-	// Set compare match value for 1ms interrupt
-	OCR2A = 249; // Assuming 16MHz clock, prescaler 64, results in 1ms period
-	// Enable Timer2 compare match A interrupt
+	TCCR2A = (1 << WGM21);   // Configura o Timer2 para o modo CTC
+	TCCR2B = (1 << CS22);	 // Set prescaler to 64 and start Timer2
+	// Configura o valor de comparação para interrupção de 1ms	
+	OCR2A = 249;			 // Assumindo clock de 16MHz, prescaler 64, resulta em período de 1ms
+	// Habilita a interrupção de comparação do Timer2
 	TIMSK2 = (1 << OCIE2A);
-	// Enable global interrupts
-	sei();
+	sei();					 // Habilita interrupções globais
 }
 
+// Reseta o tempo decorrido para o Timer2
 void reset_timer_2(void) {
 	elapsed_time_2 = 0;
 }
 
+// Verifica se o tempo decorrido é maior ou igual a 12000ms (12 segundos), utilizada na verificação de porta aberta
 char is_timeout_2(void) {
-	return (elapsed_time_2 >= 12000); // Timeout period in milliseconds
+	return (elapsed_time_2 >= 12000);
 }
 
+// Para o Timer2 limpando os bits do prescaler no TCCR2B
 void stop_timer2(void) {
-	// Clear the prescaler bits in TCCR2B to stop the timer
 	TCCR2B &= ~((1 << CS22) | (1 << CS21) | (1 << CS20));
 }
 
 // ================================= Timer 3  ================================= //
 // ================================= BUZZER ================================= //
 
+// Configura timer utilizado no controle do buzzer de porta aberta
 void init_door_buzzer(void) {
-	 // Set PE4 as output
-	 DDRE |= (1 << PE4);
-	 // Set CIRCUIT_PIN as input
-	 DDRE &= ~(1 << DOOR_PIN);
-	 // Enable pull-up resistor on CIRCUIT_PIN
-	 PORTE |= (1 << DOOR_PIN);
 	 
-	 // Configure Timer 3 for 700 Hz
-	 TCCR3A = 0; // Normal mode
-	 TCCR3B = (1 << WGM32) | (1 << CS31); // CTC mode, prescaler 8
-	 OCR3A = (F_CPU / (2 * 8 * 700)) - 1; // Set compare value for 700 Hz
-
-	 // Enable Timer 3 compare interrupt
+	 DDRE |= (1 << PE4);	    // Configura o pino PE4 como saída
+	 DDRE &= ~(1 << DOOR_PIN);  // Configura o pino DOOR_PIN como entrada
+	 PORTE |= (1 << DOOR_PIN);  // Habilita o resistor de pull-up no pino DOOR_PIN
+	 
+     // Configura o Timer 3 para 700 Hz
+	 TCCR3A = 0;						  // Modo Normal
+	 TCCR3B = (1 << WGM32) | (1 << CS31); // Modo CTC, prescaler 8
+	 OCR3A = (F_CPU / (2 * 8 * 700)) - 1; // Configura valor de comparação para 700 Hz
+	
+	 // Habilita a interrupção de comparação do Timer 3
 	 TIMSK3 |= (1 << OCIE3A);
 	 
-	 // Enable external interrupt on INT5
-	 //EICRB |= (1 << ISC51) | (1 << ISC50); // Configura INT5 para gerar interrupção em qualquer mudança de nível
-	 EICRB |= (1 << ISC51);  // Trigger on falling edge
-	 EIMSK |= (1 << INT5);   // Enable INT5
+	 // Habilita a interrupção externa no INT5
+	 EICRB |= (1 << ISC51);  // Gera interrupção na borda de descida
+	 EIMSK |= (1 << INT5);   // Habilita INT5
 }
 
 void sound_alarm(void) {
-	// Start Timer 3
-	TCCR3B |= (1 << CS31); // Start Timer3 with prescaler 8
+	TCCR3B |= (1 << CS31); // Inicia Timer3 com prescaler 8
 }
 
-
 void stop_alarm(void){
-	// Stop Timer 3 and turn off the buzzer
-	TCCR3B &= ~(1 << CS31); // Stop Timer3 by clearing the clock source
-	PORTE &= ~(1 << PE4); // Ensure buzzer is turned off
+	TCCR3B &= ~(1 << CS31); // Para o Timer3 limpando a fonte de clock
+	PORTE &= ~(1 << PE4);   // Garante que o buzzer está desligado
 }
 
 // ================================= Timer 4  ================================= //
 // ================================ DOOR LED ================================ //
 
 void init_timer4(void) {
-	// Configure Timer4 for CTC mode
 	TCCR4A = 0;
-	TCCR4B = (1 << WGM42) | (1 << CS42) | (1 << CS40); // CTC mode, prescaler 1024
-	OCR4A = (F_CPU / (1024 * 4)) - 1; // Set compare value for 2 Hz
-	TIMSK4 = (1 << OCIE4A); // Enable Timer4 compare interrupt
+	TCCR4B = (1 << WGM42) | (1 << CS42) | (1 << CS40); // Modo CTC, prescaler 1024
+	OCR4A = (F_CPU / (1024 * 4)) - 1;				   // Configura valor de comparação para 2 Hz
+	TIMSK4 = (1 << OCIE4A);                            // Habilita a interrupção de comparação do Timer4
 }
 
 void init_led_porta(void) {
-	// Set the LED pin as output
-	DDRH |= (1 << PH3);
+	DDRH |= (1 << PH3);   // Set the LED pin as output
 }
 
 void blink_led(void) {
-	// Toggle the LED pin
-	PORTH ^= (1 << PH3);
+	PORTH ^= (1 << PH3);  // Alterna o estado do pino do LED
 }
